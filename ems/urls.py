25cb5d33e9ae6a6c7cf3dfe40332e24b4d4ca691
puneet <puneetgirdhar.in@gmail.com>
2015-11-21 16:14:43 -0600

    add post event

--- ems/urls.py
+++ ems/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from .views import EventTrigger
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    url(r'', EventTrigger.as_view(), name='event'),
+    url(r'', csrf_exempt(EventTrigger.as_view()), name='event'),
 ]
--- ems/views.py
+++ ems/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import View
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+
+
 class EventTrigger(View):
 
+
     def get(self,request):
-        print HttpResponse("Sample page")
+        return HttpResponse("Sample page")
+
 
     def post(self, request):
-        print HttpResponse(request.body, content_type='application/xhtml+xml')
+        print request.body
+        return HttpResponse(request.body, content_type='application/xhtml+xml')
