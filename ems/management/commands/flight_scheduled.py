ae4a803a0562f8c0ced0d3214754adce9a779f8f
puneet <puneetgirdhar.in@gmail.com>
2015-11-22 10:17:47 -0600

    add flight scheduled

---
+++ ems/management/commands/flight_scheduled.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand, CommandError
+from ems.models import Journey, Flight
+from django.db.models import F
+from ems.event_management import Events
+
+
+class Command(BaseCommand):
+    help = " Flight Delay events "
+
+    def handle(self, *args, **kwargs):
+        # get the list of objects which are created within 1 minute
+        flights = Flight.objects.filter(status=3).exclude(convey=1)
+
+        # in all list of objects trigger event
+        for j in flights:
+            # check which event is it
+            Events.on_flight_ontime(j)
+            self.stdout.write("Successfully sent flight ontime event on %s"%(j,))
+            Flight.objects.filter(pk=j.pk).update(convey =1)
