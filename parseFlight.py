28a18f91db6704f91760a30f266374f705d0bf9b
puneet <puneetgirdhar.in@gmail.com>
2015-11-22 10:58:30 -0600

    parsing flight data

--- parseFlight.py
+++ parseFlight.py
@@ -1,12 +1,11 @@
 import xml.etree.ElementTree as ET
 import urllib2
 import django
-import flightQuery 
+import flightQuery
 import json
 from ems.models import Flight
 
 tree = None
-tree = ET.parse('tripData.xml')
 departPlaces = []
 bookingReferences = []
 departTimes = []
@@ -15,89 +14,99 @@ arrivalTimes = []
 flightCodes = []
 flights = []
 
-def read_data(data):
-	global tree
-	tree = ET.fromstring(data)
+f = open('/Users/puneet/Projects/events/tripData.xml')
 
+def read_data(data):
+    global tree
+    tree = ET.fromstring(data)
+    print tree
+
+def getroot():
+  return tree
 
 def getUniqueID():
-	root = tree.getroot()
-	root = root[2][0]
-	e_mail = root.get('value')
-	e_mail = e_mail.rstrip('>')
-	e_mail = e_mail.lstrip('<')
-	return e_mail
+    root = getroot()
+    root = root[2][0]
+    e_mail = root.get('value')
+    e_mail = e_mail.rstrip('>')
+    e_mail = e_mail.lstrip('<')
+    return e_mail
 
 def getDeparaturePlace():
-	root = tree.getroot()
-	root = root[5];
-	for item in root:
-		if(item.tag == "flight"):
-			departPlaces.append(item[5][2].text)
-		
+    root = getroot()
+    root = root[5];
+    for item in root:
+        if(item.tag == "flight"):
+            departPlaces.append(item[5][2].text)
+
 def getDepartTimes():
-	root = tree.getroot()
-	root = root[5];
-	for item in root:
-		if(item.tag == "flight"):
-		 	departTimes.append(item[5][6].text)
+    root = getroot()
+    root = root[5];
+    for item in root:
+        if(item.tag == "flight"):
+            departTimes.append(item[5][6].text)
 
 def getArrivalTimes():
-	root = tree.getroot()
-	root = root[5];
-	for item in root:
-		if(item.tag == "flight"):
-			arrivalTimes.append(item[6][6].text)
+    root = getroot()
+    root = root[5];
+    for item in root:
+        if(item.tag == "flight"):
+            arrivalTimes.append(item[6][6].text)
 
 def getArrivalPlace():
-	root = tree.getroot()
-	root = root[5];
-	for item in root:
-		if(item.tag == "flight"):
-			arrivalPlaces.append(item[6][2].text)
+    root = getroot()
+    root = root[5];
+    for item in root:
+        if(item.tag == "flight"):
+            arrivalPlaces.append(item[6][2].text)
 
 def getFlightInfo():
-	root = tree.getroot()
-	root = root[5];
-	for item in root:
-		if(item.tag == "flight"):
-			number = item[4].get('number')
-			code = item[4].get('airline-code')
-			flightcode = code + number
-			flightCodes.append(flightcode)
+    root = getroot()
+    root = root[5];
+    for item in root:
+        if(item.tag == "flight"):
+            number = item[4].get('number')
+            code = item[4].get('airline-code')
+            flightcode = code + number
+            flightCodes.append(flightcode)
 
 def getReference():
-	root = tree.getroot()
-	root = root[5]
-	for item in root:
-		if(item.tag == "flight"):
-			reference = item[1][1].text
-			bookingReferences.append(reference)
+    root = getroot()
+    root = root[5]
+    for item in root:
+        if(item.tag == "flight"):
+            reference = item[1][1].text
+            bookingReferences.append(reference)
 
 def populateFlights():
-	root = tree.getroot()
-	root = root[5]
-	for index,item in enumerate(root):
-		tempflight = Flight()
-		tempflight.source = departPlaces[index]
-		tempflight.destination = arrivalPlaces[index]
-		tempflight.airline = flightCodes[index]
-		tempflight.departure = departTimes[index]
-		tempflight.arrival = arrivalTimes[index]
-		tempflight.reference_no = bookingReferences[index]
-		flights.append(tempflight)
-		return flights
+    print tree
+    root = getroot()
+    root = root[5]
+    for index,item in enumerate(root):
+        tempflight = Flight()
+        tempflight.source = departPlaces[index]
+        tempflight.destination = arrivalPlaces[index]
+        tempflight.airline = flightCodes[index]
+        tempflight.departure = departTimes[index]
+        tempflight.arrival = arrivalTimes[index]
+        tempflight.reference_no = bookingReferences[index]
+        flights.append(tempflight)
+        return flights
 
-''' getUniqueID();
-getDeparaturePlace();
-getArrivalPlace();
-getArrivalTimes();
-getDepartTimes();
-getFlightInfo();
-getReference();
+def process(datastr):
+    read_data(datastr)
 
-populateFlights();  '''
+    getUniqueID();
+    getDeparaturePlace();
+    getArrivalPlace();
+    getArrivalTimes();
+    getDepartTimes();
+    getFlightInfo();
+    getReference();
 
+
+process(f.read())
+print populateFlights()
 #flightQuery.getFlightStatus(flights[1]);
 
 
